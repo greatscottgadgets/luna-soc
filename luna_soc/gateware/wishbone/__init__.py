27573c9bbaea94b71a19835b6e1e3b6e771d0759
Antoine van Gelder <antoine@greatscottgadgets.com>
2024-09-16 16:59:04 +0200

    repo: re-organize repository layout

--- luna_soc/gateware/wishbone/__init__.py
+++
@@ -1,9 +0,0 @@
-#
-# This file is part of LUNA.
-#
-# Copyright (c) 2023 Great Scott Gadgets <info@greatscottgadgets.com>
-# SPDX-License-Identifier: BSD-3-Clause
-
-from .memory    import *
-from .spiflash  import *
-from .sram      import *
