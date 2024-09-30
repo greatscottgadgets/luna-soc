5acd9ef2ea34ee27cbd89478fd429273ada6968b
Antoine van Gelder <antoine@greatscottgadgets.com>
2024-09-30 18:42:13 +0200

    repo: update __init__ files

--- luna_soc/gateware/__init__.py
+++ luna_soc/gateware/__init__.py
@@ -1,7 +1,5 @@
 #
 # This file is part of LUNA.
 #
-# Copyright (c) 2020 Great Scott Gadgets <info@greatscottgadgets.com>
+# Copyright (c) 2020-2024 Great Scott Gadgets <info@greatscottgadgets.com>
 # SPDX-License-Identifier: BSD-3-Clause
-
-from .lunasoc import LunaSoC
