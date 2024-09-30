18c19e428160f5fffbe09122b4dbdee1b2697a0d
Antoine van Gelder <antoine@greatscottgadgets.com>
2024-09-30 18:48:33 +0200

    cpu: port vexriscv to amaranth-soc

--- luna_soc/gateware/cpu/vexriscv.py
+++ luna_soc/gateware/cpu/vexriscv.py
@@ -4,10 +4,11 @@
 # Copyright (c) 2023 Great Scott Gadgets <info@greatscottgadgets.com>
 # SPDX-License-Identifier: BSD-3-Clause
 
-from amaranth import *
+from amaranth             import *
+from amaranth.lib.wiring  import Component, In, Out
 
-from ..vendor.amaranth_soc import wishbone
-from ..vendor.amaranth_soc.periph import ConstantMap
+from amaranth_soc         import wishbone
+from amaranth_soc.periph  import ConstantMap
 
 import os
 import logging
@@ -25,39 +26,44 @@ JTAG_VARIANTS = [ "cynthion+jtag" ]
 
 # - VexRiscv ------------------------------------------------------------------
 
-class VexRiscv(Elaboratable):
-    name       = "vexriscv"
-    arch       = "riscv"
-    byteorder  = "little"
-    data_width = 32
+class VexRiscv(Component):
+    #name       = "vexriscv"
+    #arch       = "riscv"
+    #byteorder  = "little"
+    #data_width = 32
 
-    def __init__(self,
-                 variant="imac+dcache",
-                 reset_addr=0x00000000):
-        super().__init__()
-
+    def __init__(self, variant="imac+dcache", reset_addr=0x00000000):
         self._variant    = variant
         self._reset_addr = reset_addr
 
-        # ports
-        self.ext_reset    = Signal()
-        self.irq_external = Signal(32)
-        self.irq_timer    = Signal()
-        self.irq_software = Signal()
-        self.ibus = wishbone.Interface(addr_width=30, data_width=32, granularity=8,
-                                       features={"cti", "bte", "err"})
-        self.dbus = wishbone.Interface(addr_width=30, data_width=32, granularity=8,
-                                       features={"cti", "bte", "err"})
+        super().__init__({
+            "ext_reset":     In(unsigned(1)),
 
-        # optional jtag port
-        if variant in JTAG_VARIANTS:
-            self.jtag_tms  = Signal()
-            self.jtag_tdi  = Signal()
-            self.jtag_tdo  = Signal()
-            self.jtag_tck  = Signal()
-            self.dbg_reset = Signal()
-            self.ndm_reset = Signal()
-            self.stop_time = Signal()
+            "irq_external":  In(unsigned(32)),
+            "irq_timer":     In(unsigned(1)),
+            "irq_software":  In(unsigned(1)),
+
+            "ibus": Out(wishbone.Signature(
+                addr_width=30,
+                data_width=32,
+                granularity=8,
+                features=("err", "cti", "bte")
+            )),
+            "dbus": Out(wishbone.Signature(
+                addr_width=30,
+                data_width=32,
+                granularity=8,
+                features=("err", "cti", "bte")
+            )),
+
+            "jtag_tms":  In(unsigned(1)),
+            "jtag_tdi":  In(unsigned(1)),
+            "jtag_tdo":  Out(unsigned(1)),
+            "jtag_tck":  In(unsigned(1)),
+            "dbg_reset": In(unsigned(1)),
+            "ndm_reset": In(unsigned(1)),
+            "stop_time": In(unsigned(1)),
+        })
 
         # read source verilog
         if not variant in CPU_VARIANTS:
@@ -70,42 +76,38 @@ class VexRiscv(Elaboratable):
             logging.info(f"reading verilog file: {self._source_path}")
             self._source_verilog = f.read()
 
-    @property
-    def reset_addr(self):
-        return self._reset_addr
+    #@property
+    #def reset_addr(self):
+    #    return self._reset_addr
 
-    @property
-    def muldiv(self):
-        return "hard" # "hard" if self._cpu.with_muldiv else "soft"
+    #@property
+    #def muldiv(self):
+    #    return "hard" # "hard" if self._cpu.with_muldiv else "soft"
 
-    @property
-    def constant_map(self):
-        return ConstantMap(
-            VEXRISCV          = True,
-            RESET_ADDR        = self._reset_addr,
-            ARCH_RISCV        = True,
-            RISCV_MULDIV_SOFT = self.muldiv == "soft",
-            BYTEORDER_LITTLE  = True,
-        )
+    #@property
+    #def constant_map(self):
+    #    return ConstantMap(
+    #        VEXRISCV          = True,
+    #        RESET_ADDR        = self._reset_addr,
+    #        ARCH_RISCV        = True,
+    #        RISCV_MULDIV_SOFT = self.muldiv == "soft",
+    #        BYTEORDER_LITTLE  = True,
+    #    )
 
     def elaborate(self, platform):
         m = Module()
-
-        # signals
-        i_clk       = ClockSignal("sync")
-        i_reset     = ResetSignal("sync") | self.ext_reset
 
         # optional signals
         optional_signals = {}
         if self._variant in JTAG_VARIANTS:
             optional_signals = {
-                "i_jtag_tms":   self.jtag_tms,
-                "i_jtag_tdi":   self.jtag_tdi,
-                "o_jtag_tdo":   self.jtag_tdo,
-                "i_jtag_tck":   self.jtag_tck,
-                "i_debugReset": self.dbg_reset,
-                "o_ndmreset":   self.ndm_reset,
-                "o_stoptime":   self.stop_time,
+                "i_jtag_tms":       self.jtag_tms,
+                "i_jtag_tdi":       self.jtag_tdi,
+                "o_jtag_tdo":       self.jtag_tdo,
+                "i_jtag_tck":       self.jtag_tck,
+                "i_debugReset":     self.dbg_reset,
+                "o_ndmreset":       self.ndm_reset,
+                "o_stoptime":       self.stop_time,
             }
 
         # instantiate VexRiscv
@@ -114,9 +116,9 @@ class VexRiscv(Elaboratable):
             "VexRiscv",
 
             # clock and reset
-            i_clk                    = i_clk,
-            i_reset                  = i_reset,
-            i_externalResetVector    = Signal(32, reset=self._reset_addr),
+            i_clk                    = ClockSignal("sync"),
+            i_reset                  = ResetSignal("sync") | self.ext_reset,
+            i_externalResetVector    = Const(self._reset_addr, unsigned(32)),
 
             # interrupts
             i_externalInterruptArray = self.irq_external,
